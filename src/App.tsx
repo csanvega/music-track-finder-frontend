4d99fc5b37ee089b0b8f0b319edbfcb642aa4fdd
Carlos Santander <csantanderv@gmail.com>
2025-05-10 20:39:09 -0400

    chore: add material ui, redux, react-router, styled-components libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
+import Button from '@mui/material/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>Initial Setup</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1>Initial Setup</h1>
+      <div>
+        <Button
+          variant="contained"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
-        </button>
+        </Button>
       </div>
     </>
   )
